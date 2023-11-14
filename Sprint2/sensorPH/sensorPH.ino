b44f5ff291c454db6c2433bc71335bb6d416ab79
victormnt37 <victormorantf@gmail.com>
2023-11-14 13:06:05 +0100

    para pablo

--- Sprint2/sensorPH/sensorPH.ino
+++ Sprint2/sensorPH/sensorPH.ino
@@ -1,3 +1,4 @@
+#include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
 // Definir el Canal del ADS1115 por el que leeremos la tension del sensor de PH
@@ -9,17 +10,18 @@ int pHArray[ArrayLength]; // almacena las muestras
 int pHArrayIndex = 0;
 
 // Construimos el ADS1115
-// TO-DO
+Adafruit_ADS1115 ads; // TO-DO: Instanciar el objeto de la clase Adafruit_ADS1115
 
 void setup() {
-  // Inicializamos  el monitor serie
+  // Inicializamos el monitor serie
   Serial.begin(9600);
   Serial.println("Inicializando el medidor de PH");
 
   // Inicializamos el ADS1115
-  // TO-DO
+  ads.begin(); // TO-DO: Inicializar el objeto ADS1115
+
   // Configuramos la ganancia del ADS1115
-  // TO-DO
+  ads.setGain(GAIN_ONE); // TO-DO: Configurar la ganancia del ADS1115
 }
 
 void loop() {
@@ -30,10 +32,10 @@ void loop() {
 
   if (millis() - samplingTime > samplingInterval) {
     // realizar una lectura del ADS11115
-    pHArray[pHArrayIndex++] = //TO-DO;
-    if (pHArrayIndex == ArrayLenght) pHArrayIndex = 0;
+    pHArray[pHArrayIndex++] = ads.readADC_SingleEnded(channelValue); // TO-DO: Realizar la lectura del canal del ADS1115
+    if (pHArrayIndex == ArrayLength) pHArrayIndex = 0;
     // convertir la tectura en tensión
-    voltage = //TO-DO;
+    voltage = (float)pHArray[pHArrayIndex] * 5.0 / 32767.0; // TO-DO: Convertir la lectura a tensión
     pHValue = 3.5 * voltage + Offset;
     samplingTime = millis();
   }
