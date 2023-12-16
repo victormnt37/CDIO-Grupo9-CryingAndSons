1994035d09fca3f28da68fcea171acdb3ea7c581
Diabolo-kun <103450138+Diabolo-kun@users.noreply.github.com>
2023-12-16 11:52:12 +0100

    conexion a internet

--- ProgramaSensoresConjunto/ProgramaSensoresConjunto.ino
+++ ProgramaSensoresConjunto/ProgramaSensoresConjunto.ino
@@ -22,8 +22,8 @@ int MaxSensor = 650;   // valor en mojado
 // Inicialización variables PH
 #define Offset 0.00
 #define samplingInterval 20
-#define ArrayLength 40 // numero de muestras
-int pHArray[ArrayLength]; // almacena las muestras
+#define ArrayLength 40     // numero de muestras
+int pHArray[ArrayLength];  // almacena las muestras
 int pHArrayIndex = 0;
 
 // Iniciacion de variables de pines
@@ -34,6 +34,187 @@ String Pin0, Pin1, Pin2, Pin3;
 String listaPines[] = { Pin0, Pin1, Pin2, Pin3 };  //se autoactualiza?
 
 #define power_pin 5  // Pin para alimentar el sensor de salinidad
+
+#include <ESP8266WiFi.h>
+
+// Comentar/Descomentar para ver mensajes de depuracion en monitor serie y/o respuesta del HTTP server
+#define PRINT_DEBUG_MESSAGES
+#define PRINT_HTTP_RESPONSE
+
+// Comentar/Descomentar para conexion Fuera/Dentro de UPV
+//#define WiFi_CONNECTION_UPV
+
+// Selecciona que servidor REST quieres utilizar entre ThingSpeak y Dweet
+//#define REST_SERVER_THINGSPEAK //Selecciona tu canal para ver los datos en la web (https://thingspeak.com/channels/360979)
+#define REST_SERVER_DWEET  //Selecciona tu canal para ver los datos en la web (http://dweet.io/follow/PruebaGTI)
+
+///////////////////////////////////////////////////////
+/////////////// WiFi Definitions /////////////////////
+//////////////////////////////////////////////////////
+
+#ifdef WiFi_CONNECTION_UPV  //Conexion UPV
+const char WiFiSSID[] = "GTI1";
+const char WiFiPSK[] = "1PV.arduino.Toledo";
+#else  //Conexion fuera de la UPV
+const char WiFiSSID[] = "aaaa";
+const char WiFiPSK[] = "12345678";
+#endif
+
+
+
+///////////////////////////////////////////////////////
+/////////////// SERVER Definitions /////////////////////
+//////////////////////////////////////////////////////
+
+#if defined(WiFi_CONNECTION_UPV)  //Conexion UPV
+const char Server_Host[] = "proxy.upv.es";
+const int Server_HttpPort = 8080;
+#elif defined(REST_SERVER_THINGSPEAK)  //Conexion fuera de la UPV
+const char Server_Host[] = "api.thingspeak.com";
+const int Server_HttpPort = 80;
+#else
+const char Server_Host[] = "dweet.io";
+const int Server_HttpPort = 80;
+#endif
+
+WiFiClient client;
+
+///////////////////////////////////////////////////////
+/////////////// HTTP REST Connection ////////////////
+//////////////////////////////////////////////////////
+
+#ifdef REST_SERVER_THINGSPEAK
+const char Rest_Host[] = "api.thingspeak.com";
+String MyWriteAPIKey = "15CUR2E6XRSC3G24";  // Escribe la clave de tu canal ThingSpeak
+#else
+const char Rest_Host[] = "dweet.io";
+String MyWriteAPIKey = "cdiocurso2018g09";  // Escribe la clave de tu canal Dweet
+#endif
+
+#define NUM_FIELDS_TO_SEND 5  //Numero de medidas a enviar al servidor REST (Entre 1 y 8)
+
+/////////////////////////////////////////////////////
+/////////////// Pin Definitions ////////////////
+//////////////////////////////////////////////////////
+
+const int LED_PIN = 5;  // Thing's onboard, green LED
+
+/////////////////////////////////////////////////////
+/////////////// WiFi Connection ////////////////
+//////////////////////////////////////////////////////
+
+void connectWiFi() {
+  byte ledStatus = LOW;
+
+#ifdef PRINT_DEBUG_MESSAGES
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+#endif
+
+  WiFi.begin(WiFiSSID, WiFiPSK);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    // Blink the LED
+    digitalWrite(LED_PIN, ledStatus);  // Write LED high/low
+    ledStatus = (ledStatus == HIGH) ? LOW : HIGH;
+#ifdef PRINT_DEBUG_MESSAGES
+    Serial.println(".");
+#endif
+    delay(500);
+  }
+#ifdef PRINT_DEBUG_MESSAGES
+  Serial.println("WiFi Connected");
+  Serial.println(WiFi.localIP());  // Print the IP address
+#endif
+}
+
+/////////////////////////////////////////////////////
+/////////////// HTTP POST  ThingSpeak////////////////
+//////////////////////////////////////////////////////
+void HTTPPost(String fieldData[], int numFields) {
+  if (client.connect(Server_Host, Server_HttpPort)) {
+
+    // Construimos el string de datos. Si tienes multiples campos asegurate de no pasarte de 1440 caracteres
+
+    String PostData = "api_key=" + MyWriteAPIKey;
+    for (int field = 1; field < (numFields + 1); field++) {
+      PostData += "&field" + String(field) + "=" + fieldData[field];
+    }
+
+// POST data via HTTP
+#ifdef PRINT_DEBUG_MESSAGES
+    Serial.println("Connecting to ThingSpeak for update...");
+#endif
+    client.println("POST http://" + String(Rest_Host) + "/update HTTP/1.1");
+    client.println("Host: " + String(Rest_Host));
+    client.println("Connection: close");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Content-Length: " + String(PostData.length()));
+    client.println();
+    client.println(PostData);
+#ifdef PRINT_DEBUG_MESSAGES
+    Serial.println(PostData);
+    Serial.println();
+//Para ver la respuesta del servidor
+#ifdef PRINT_HTTP_RESPONSE
+    delay(500);
+    Serial.println();
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+    Serial.println();
+    Serial.println();
+#endif
+#endif
+  }
+}
+
+////////////////////////////////////////////////////
+/////////////// HTTP GET  ////////////////
+//////////////////////////////////////////////////////
+void HTTPGet(String fieldData[], int numFields) {
+  if (client.connect(Server_Host, Server_HttpPort)) {
+#ifdef REST_SERVER_THINGSPEAK
+    String PostData = "GET https://api.thingspeak.com/update?api_key=";
+    PostData = PostData + MyWriteAPIKey;
+#else
+    String PostData = "GET http://dweet.io/dweet/for/";
+    PostData = PostData + MyWriteAPIKey + "?";
+#endif
+
+    for (int field = 1; field < (numFields + 1); field++) {
+      PostData += "&field" + String(field) + "=" + fieldData[field];
+    }
+
+
+#ifdef PRINT_DEBUG_MESSAGES
+    Serial.println("Connecting to Server for update...");
+#endif
+    client.print(PostData);
+    client.println(" HTTP/1.1");
+    client.println("Host: " + String(Rest_Host));
+    client.println("Connection: close");
+    client.println();
+#ifdef PRINT_DEBUG_MESSAGES
+    Serial.println(PostData);
+    Serial.println();
+//Para ver la respuesta del servidor
+#ifdef PRINT_HTTP_RESPONSE
+    delay(500);
+    Serial.println();
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+    Serial.println();
+    Serial.println();
+#endif
+#endif
+  }
+}
+
+
 
 // ****************************** Definición de funciones ******************************
 
@@ -102,7 +283,7 @@ float medirPH(int pin) {
     // realizar varias lecturas del ADS11115
     for (int i = 0; i < ArrayLength; i++) {
       pHArray[i] = ads.readADC_SingleEnded(pin);
-      delay(2); // espera pequeña entre lecturas para estabilizar
+      delay(2);  // espera pequeña entre lecturas para estabilizar
     }
     // calcular la media de las muestras
     voltage = averageSample(ArrayLength, pHArray) * 5.0 / 32767.0;
@@ -203,7 +384,12 @@ void exeMenuCalibracion() {
 // ****************************** SETUP ******************************
 
 void setup() {
-  Serial.begin(115200);
+
+  // Serial.begin(115200);
+
+#ifdef PRINT_DEBUG_MESSAGES
+  Serial.begin(9600);
+#endif
   Serial.println("Programa de lectura de NTC con ESP8266 y ADS1115");
 
   if (!ads.begin()) {  // Inicializar el ADC para la humedad
@@ -219,7 +405,7 @@ void setup() {
   delay(1000);
   leerUltimaSonda();
   infoSonda();
-  
+
   // Preguntar al usuario si desea ejecutar el menú
   Serial.println("¿Desea ejecutar el menú? (y/n)");
 
@@ -234,29 +420,85 @@ void setup() {
     exeMenuCalibracion();
   } else {
     Serial.println("Iniciando.");
-  }
+    connectWiFi();
+    digitalWrite(LED_PIN, HIGH);
 
+#ifdef PRINT_DEBUG_MESSAGES
+    Serial.print("Server_Host: ");
+    Serial.println(Server_Host);
+    Serial.print("Port: ");
+    Serial.println(String(Server_HttpPort));
+    Serial.print("Server_Rest: ");
+    Serial.println(Rest_Host);
+#endif
+  }
 }
 
 // ****************************** Loop ******************************
 
 void loop() {
-  //medir Temperatura y imprimir
+  // //medir Temperatura y imprimir
+  // Serial.print(": ");
+  // Serial.println();
+  // //medir Salinidad y imprimir
+  // float salinidad = ;
+  // Serial.print("Salinidad: ");
+  // Serial.println(salinidad);
+  // //medir humedad y imprimir
+  // Serial.print("Humedad: ");
+  // Serial.println();
+  // //medir ph y imprimir
+  // Serial.print("PH: ");
+  // Serial.println();
+  // //medir luz y imprimir
+  // Serial.print("LUM: ");
+  // //Serial.println();
+  Serial.println("______________________________________________");
+  //delay(1000);
+  // Serial.println("\033[2J\033[H");  //borrar monitor serie
+
+  String data[NUM_FIELDS_TO_SEND + 1];  // Podemos enviar hasta 8 datos
+
+
+  data[1] = String(tomarTemperatura(1));  //1=num pin de termometro, debuelve un float //Escribimos el dato 1. Recuerda actualizar numFields
+#ifdef PRINT_DEBUG_MESSAGES
   Serial.print("Temperatura: ");
-  Serial.println(tomarTemperatura(1));//1=num pin de termometro, debuelve un float
-  //medir Salinidad y imprimir
-  float salinidad = medirSalinidad();
+  Serial.println(data[1]);
+#endif
+
+  data[2] = String(medirSalinidad());  //Escribimos el dato 2. Recuerda actualizar numFields
+#ifdef PRINT_DEBUG_MESSAGES
   Serial.print("Salinidad: ");
-  Serial.println(salinidad);
-  //medir humedad y imprimir
+  Serial.println(data[2]);
+#endif
+
+
+  data[3] = String(medirHumedad(0));  //0=num pin de termometro, debuelve un int //Escribimos el dato 3. Recuerda actualizar numFields
+#ifdef PRINT_DEBUG_MESSAGES
   Serial.print("Humedad: ");
-  Serial.println(medirHumedad(0));//0=num pin de termometro, debuelve un int
-  //medir ph y imprimir
+  Serial.println(data[3]);
+#endif
+
+
+  data[4] = String(medirPH(2));  //2=num pin de termometro, debuelve un float //Escribimos el dato 2. Recuerda actualizar numFields
+#ifdef PRINT_DEBUG_MESSAGES
   Serial.print("PH: ");
-  Serial.println(medirPH(2));//2=num pin de termometro, debuelve un float
-  //medir luz y imprimir
-  Serial.print("LUM: ");
-  //Serial.println(medirLuz(3));//3=num pin de termometro, debuelve un float
-  Serial.println("______________________________________________");  delay(1000);
-  Serial.println("\033[2J\033[H");//borrar monitor serie
+  Serial.println(data[4]);
+#endif
+
+  //data[ 5 ] = String( medirLuz(3) ); //3=num pin de termometro, debuelve un float //Escribimos el dato 2. Recuerda actualizar numFields
+  data[5] = String(0);  //quitar cuando haya sensor de luz
+#ifdef PRINT_DEBUG_MESSAGES
+  Serial.print("Luz: ");
+  Serial.println(data[5]);
+#endif
+
+  //Selecciona si quieres enviar con GET(ThingSpeak o Dweet) o con POST(ThingSpeak)
+  //HTTPPost( data, NUM_FIELDS_TO_SEND );
+  HTTPGet(data, NUM_FIELDS_TO_SEND);
+
+  //Selecciona si quieres un retardo de 15seg para hacer pruebas o dormir el SparkFun
+  delay(15000);
+  //Serial.print( "Goodnight" );
+  //ESP.deepSleep( sleepTimeSeconds * 1000000 );
 }
