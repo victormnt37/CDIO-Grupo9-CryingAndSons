ef0842c5972d3ae819e681ae3cfa7aa17319e6c5
victormnt37 <victormorantf@gmail.com>
2023-12-17 11:20:07 +0100

    lectura datos
    ya se contacta con el api

--- AppWeb/main.js
+++ AppWeb/main.js
@@ -1,14 +1,37 @@
-const cursoCDIOgrupo = 'cdiocurso2023g09';
+const url = 'https://dweet.io:443/get/dweets/for/cdiocurso2023g09';
+
+let valorHumedad = 'Inactivo';
+let valorTemperatura = 'Inactivo';
+let valorSalinidad = 'Inactivo';
+let valorPH = 'Inactivo';
+let valorLuz = 'Inactivo';
 
 //dweet.io/dweet/for/cdiocurso2023g09
 //dweet.io:443/get/dweets/for/
 
-getData();
+// getData();
 
-setInterval(getData(), 3000);
+// setInterval(getData(), 3000);
 
-async function getData() {
-  const resp = await fetch('https://dweet.io/dweet/for/' + cursoCDIOgrupo);
-  console.log(resp);
-  console.log(resp.body); // locked?
-}
+// async function getData() {
+//   const resp = await fetch(url);
+//   console.log(resp);
+//   console.log(resp.body); // locked?
+// }
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data.with[0].content); // el 0 ha de cambiar al último posible? o siempre es el 0, una de dos
+    console.log(data.with[0].content);
+    valorHumedad = data.with[0].content.field1; // Temperatura
+    valorSalinidad = data.with[0].content.field2; // Salinidad
+    valorHumedad = data.with[0].content.field3; // Humedad
+    valorHumedad = data.with[0].content.field4; // PH
+    valorLuz = data.with[0].content.field5; // Luz
+  })
+  .catch(function (error) {
+    console.log('Hubo un problema con la solicitud fetch: ', error);
+  });
